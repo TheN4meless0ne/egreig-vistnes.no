dbff6cc7c2d046b65a8fb3b20dcb16ad5545376a
Elias Greig-Vistnes <159443781+TheN4meless0ne@users.noreply.github.com>
2025-12-08 21:00:59 +0100

    Update PanelImage to accept dynamic body text props

--- website/app/components/panelImage.tsx
+++ website/app/components/panelImage.tsx
@@ -1,20 +1,20 @@
 import Image from "./image";
 import type { FilterDims } from "../types/filters";
 
-export default function PanelImage({ heading, subheading }: FilterDims) {
+export default function PanelImage({ heading, subheading, body1, body2 }: FilterDims) {
     return (
         <div className="w-full max-w-[1200px] px-4 py-8 inline-flex justify-start items-center gap-12">
-            <img className="h-80" src="https://placehold.co/1024x768" />
+            <Image alt="Panel Image" src="https://placehold.co/426x320" />
             <div className="h-80 min-w-72 inline-flex flex-col justify-start items-start gap-6">
                 <div className="self-stretch flex flex-col justify-start items-start gap-2">
-                    <div className="self-stretch justify-start text-Text-Default-Default text-2xl font-semibold font-['Inter'] leading-7">Heading</div>
-                    <div className="self-stretch justify-center text-Text-Default-Secondary text-xl font-normal font-['Inter'] leading-6">Subheading</div>
+                    <div className="self-stretch justify-start text-2xl font-semibold leading-7">{heading}</div>
+                    <div className="self-stretch justify-center text-xl font-normal leading-6">{subheading}</div>
                 </div>
                 <div className="self-stretch inline-flex justify-start items-start">
-                    <div className="flex-1 justify-start text-Text-Default-Default text-base font-normal font-['Inter'] leading-6">Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look:</div>
+                    <div className="flex-1 justify-start text-base font-normal leading-6">{body1}</div>
                 </div>
                 <div className="self-stretch inline-flex justify-start items-start">
-                    <div className="flex-1 justify-start text-Text-Default-Default text-base font-normal font-['Inter'] leading-6">Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui  international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur.</div>
+                    <div className="flex-1 justify-start text-base font-normal leading-6">{body2}</div>
                 </div>
             </div>
         </div>
