f7c3b572e2b7ea84d15db1e3da1284ba500d64b1
Elias Greig-Vistnes <eliasgr@egreig-vistnes.no>
2026-06-04 09:57:55 +0200

    Ensure webapp can be run in dev mode from code-server

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  assetPrefix: isProd ? undefined : "/proxy/3000",
   async rewrites() {
     return [
       {
