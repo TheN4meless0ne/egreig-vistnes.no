10239160e86e05bd3e6080898ad8b2d8d8221e14
Elias Greig-Vistnes <159443781+TheN4meless0ne@users.noreply.github.com>
2026-08-13 12:15:01 +0200

    Update pages

--- website/app/portfolio/page.tsx
+++ website/app/portfolio/page.tsx
@@ -5,8 +5,8 @@ export const metadata = {
 
 export default function PortfolioPage() {
     return (
-        <main className="flex min-h-screen items-center justify-center">
-            <h1 className="text-2xl font-semibold">Portfolio</h1>
+        <main className="flex items-center justify-center">
+            <h1 className="text-2xl font-semibold">🚧 This page is currently under development 🚧</h1>
         </main>
     );
 }
--- website/app/socials/page.tsx
+++ website/app/socials/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 export default function SocialsPage() {
   return (
     <main className="flex items-center justify-center">
-      <h1 className="text-2xl font-semibold">🚧 This page is currently under development 🚧</h1>
+        <h1 className="text-2xl font-semibold">🚧 This page is currently under development 🚧</h1>
     </main>
   );
 }
